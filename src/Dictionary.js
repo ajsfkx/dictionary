55fa1525ad2d4690fe3aaf1383a7f045b43b4414
ajsfkx <ajs.fkx@gmail.com>
2023-01-28 13:23:18 -0400

    Rendered the meaning

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
@@ -23,10 +24,8 @@ export default function Dictionary() {
     <div className="Dictionary">
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
-        <a href="/" className="btn btn-primary">
-          Search{" "}
-        </a>
       </form>
+      <Results results={results} />
     </div>
   );
 }
